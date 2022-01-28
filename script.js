872eec3bd495a52c016098925d5977584b0acceb
Parker Rudd <parkerbrudd@gmail.com>
2022-01-27 21:31:44 -0700

    added hours incrementation

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ let secondsArr = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', 10
 let secondsIndex = 55; 
 
 let minutesArr = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59];
-let minutesIndex = 58; 
+let minutesIndex = 59; 
 
 let hoursArr = ['01', '02', '03', '04', '05', '06', '07', '08', '09', 10, 11, 12]; 
-let hoursIndex = 0; 
+let hoursIndex = 11; 
 
 
 //INCREMENT SECONDS 
@@ -50,3 +50,4 @@ function incrementHours() {
     }
 }
 
+console.log(hoursArr.length); 

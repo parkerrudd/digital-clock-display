ad7b9949c76318b7f006c03f1a824f9107b3ae06
Parker Rudd <parkerbrudd@gmail.com>
2022-01-27 18:41:02 -0700

    changed the colors and added the increment hours function

--- script.js
+++ script.js
@@ -14,7 +14,10 @@ let secondsArr = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', 10
 let secondsIndex = 55; 
 
 let minutesArr = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59];
-let minutesIndex = 0; 
+let minutesIndex = 58; 
+
+let hoursArr = ['01', '02', '03', '04', '05', '06', '07', '08', '09', 10, 11, 12]; 
+let hoursIndex = 0; 
 
 
 //INCREMENT SECONDS 
@@ -32,10 +35,18 @@ function incrementSeconds() {
 //INCREMENT MINUTES
 function incrementMinutes() {
     minutes.innerText = minutesArr[minutesIndex++]; 
-        if (minutesIndex == minutesIndex.length) {
+        if (minutesIndex == minutesArr.length) {
+            setTimeout(incrementHours, 60000); 
             return minutesIndex = 0; 
-
         }
 }; 
 
+
+//INCREMENT HOURS 
+function incrementHours() {
+    hours.innerText = hoursArr[hoursIndex++]; 
+    if (hoursIndex == hoursArr.length) {
+        return hoursIndex = 0; 
+    }
+}
 
